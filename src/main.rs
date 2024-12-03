73835b7198912d0eb87fbd6295b3b98e1a69d951
TheLDB <ldb@erikboles.com>
2024-12-02 21:43:28 -0700

    day 2

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use day_one::solution::DayOne;
-
+use day_two::solution::DayTwo;
 mod day_one;
-
+mod day_two;
 enum Days {
     One,
     Two,
@@ -37,102 +37,45 @@ enum Days {
 }
 
 fn main() {
-    let day = Days::One;
-    
+    let day = Days::Two;
+
     match day {
         Days::One => {
             let _day_one_solution = DayOne::solution();
             let _day_one_solution_p2 = DayOne::solution_p2();
-        },
-        Days::Two => {
-
-        },
-        Days::Three => {
-
-        },
-        Days::Four => {
-
-        },
-        Days::Five => {
-
-        },
-        Days::Six => {
-
-        },
-        Days::Seven => {
-
-        },
-        Days::Eight => {
-
-        },
-        Days::Nine => {
-
-        },
-        Days::Ten => {
-
-        },
-        Days::Eleven => {
-
-        },
-        Days::Twelve => {
-
-        },
-        Days::Thirteen => {
-
-        },
-        Days::Fourteen => {
-
-        },
-        Days::Fifteen => {
-
-        },
-        Days::Sixteen => {
-
-        },
-        Days::Seventeen => {
-
-        },
-        Days::Eighteen => {
-
-        },
-        Days::Nineteen => {
-
-        },
-        Days::Twenty => {
-
-        },
-        Days::TwentyOne => {
-
-        },
-        Days::TwentyTwo => {
-
-        },
-        Days::TwentyThree => {
-
-        },
-        Days::TwentyFour => {
-
-        },
-        Days::TwentyFive => {
-
-        },
-        Days::TwentySix => {
-
-        },
-        Days::TwentySeven => {
-
-        },
-        Days::TwentyEight => {
-
-        },
-        Days::TwentyNine => {
-
-        },
-        Days::Thirty => {
-
-        },
-        Days::ThirtyOne => {
-
         }
+        Days::Two => {
+            DayTwo::part_one();
+            DayTwo::part_two();
+        }
+        Days::Three => {}
+        Days::Four => {}
+        Days::Five => {}
+        Days::Six => {}
+        Days::Seven => {}
+        Days::Eight => {}
+        Days::Nine => {}
+        Days::Ten => {}
+        Days::Eleven => {}
+        Days::Twelve => {}
+        Days::Thirteen => {}
+        Days::Fourteen => {}
+        Days::Fifteen => {}
+        Days::Sixteen => {}
+        Days::Seventeen => {}
+        Days::Eighteen => {}
+        Days::Nineteen => {}
+        Days::Twenty => {}
+        Days::TwentyOne => {}
+        Days::TwentyTwo => {}
+        Days::TwentyThree => {}
+        Days::TwentyFour => {}
+        Days::TwentyFive => {}
+        Days::TwentySix => {}
+        Days::TwentySeven => {}
+        Days::TwentyEight => {}
+        Days::TwentyNine => {}
+        Days::Thirty => {}
+        Days::ThirtyOne => {}
     }
 }
