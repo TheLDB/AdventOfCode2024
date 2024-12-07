b92def43d0e3cccd21808978cc695e41c2259929
TheLDB <ldb@erikboles.com>
2024-12-07 15:05:22 -0700

    yikes

--- src/solutions/day_6/solution.rs
+++ src/solutions/day_6/solution.rs
@@ -20,20 +20,33 @@ struct Turn {
 }
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
-struct Point {
-    x: i32,
-    y: i32,
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Guard {
     direction: GuardDirection,
     coords: Point,
 }
 
+#[derive(Clone)]
 struct Map {
     guard: Guard,
     map: Vec<Vec<char>>,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
+struct State {
+    coords: Point,
+    direction: GuardDirection,
+}
+
+#[derive(Debug, Hash, Eq, PartialEq, Clone, Copy)]
+struct ObstructionHit {
+    point: Point,
+    direction: GuardDirection,
 }
 
 impl D6 {
@@ -68,6 +81,121 @@ impl D6 {
             map: lines,
         }
     }
+
+    fn simulate_guard(&self, mut map: Map, obstruction: Point) -> bool {
+        let (mut guard, mut grid) = (map.guard, map.map);
+        let mut obstruction_hits: HashSet<ObstructionHit> = HashSet::new();
+        let max_steps = 20000;
+        let mut steps = 0;
+
+        loop {
+            steps += 1;
+            if steps > max_steps {
+                return true;
+            }
+
+            let next_point = match guard.direction {
+                GuardDirection::Up => Point {
+                    x: guard.coords.x,
+                    y: guard.coords.y - 1,
+                },
+                GuardDirection::Down => Point {
+                    x: guard.coords.x,
+                    y: guard.coords.y + 1,
+                },
+                GuardDirection::Left => Point {
+                    x: guard.coords.x - 1,
+                    y: guard.coords.y,
+                },
+                GuardDirection::Right => Point {
+                    x: guard.coords.x + 1,
+                    y: guard.coords.y,
+                },
+            };
+
+            if next_point == obstruction {
+                let hit = ObstructionHit {
+                    point: obstruction,
+                    direction: guard.direction,
+                };
+
+                if !obstruction_hits.insert(hit) {
+                    println!("Loop detected! Guard hit obstruction at ({}, {}) twice from {:?} direction",
+                        obstruction.x, obstruction.y, guard.direction);
+                    return true;
+                }
+            }
+
+            match guard.direction {
+                GuardDirection::Up => {
+                    let above = guard.coords.y - 1;
+                    if above < 0 {
+                        break;
+                    }
+
+                    let above_char = grid[above as usize][guard.coords.x as usize];
+                    if above_char == '#' {
+                        guard.direction = GuardDirection::Right;
+                    } else if above_char == '.' {
+                        grid[guard.coords.y as usize][guard.coords.x as usize] = '.';
+                        guard.coords.y -= 1;
+                    }
+                }
+                GuardDirection::Down => {
+                    let below = guard.coords.y + 1;
+                    if below >= grid.len() as i32 {
+                        break;
+                    }
+
+                    let below_char = grid[below as usize][guard.coords.x as usize];
+                    if below_char == '#' {
+                        guard.direction = GuardDirection::Left;
+                    } else if below_char == '.' {
+                        grid[guard.coords.y as usize][guard.coords.x as usize] = '.';
+                        guard.coords.y += 1;
+                    }
+                }
+                GuardDirection::Left => {
+                    let left = guard.coords.x - 1;
+                    if left < 0 {
+                        break;
+                    }
+
+                    let left_char = grid[guard.coords.y as usize][left as usize];
+                    if left_char == '#' {
+                        guard.direction = GuardDirection::Up;
+                    } else if left_char == '.' {
+                        grid[guard.coords.y as usize][guard.coords.x as usize] = '.';
+                        guard.coords.x -= 1;
+                    }
+                }
+                GuardDirection::Right => {
+                    let right = guard.coords.x + 1;
+                    if right >= grid[0].len() as i32 {
+                        break;
+                    }
+
+                    let right_char = grid[guard.coords.y as usize][right as usize];
+                    if right_char == '#' {
+                        guard.direction = GuardDirection::Down;
+                    } else if right_char == '.' {
+                        grid[guard.coords.y as usize][guard.coords.x as usize] = '.';
+                        guard.coords.x += 1;
+                    }
+                }
+            }
+
+            if guard.coords.y < 0
+                || guard.coords.y >= grid.len() as i32
+                || guard.coords.x < 0
+                || guard.coords.x >= grid[0].len() as i32
+            {
+                return false;
+            }
+        }
+
+        false
+    }
 }
 
 impl Runner for D6 {
@@ -84,7 +212,7 @@ impl Runner for D6 {
     // and while this is true, we move the guard up/down/left/right while making sure that new point isnt an obstacle
     //
     fn part_one(&self) -> Option<usize> {
-        let input = self.load_input(false);
+        let input = self.load_input(true);
         let map = self.build_map(input);
         let (mut guard, mut map) = (map.guard, map.map);
         let mut visited_spots: HashSet<Point> = HashSet::new();
@@ -162,10 +290,39 @@ impl Runner for D6 {
 
     // Ok so I need to figure out how i can trap the guard in an infinite loop
     // by placing a single obstruction (not where they are standing)
-    //
-    // i think bruteforcing is the only relatively easy way to accomplish this :(
-    // i tried to do some thing that predicted it based off the last 3 turns but it failed    
+    // this is maybe some of the worst code ever
+    // got impatient and *heavily* bruteforced it
+    // this should be like 100x faster
     fn part_two(&self) -> Option<usize> {
-        None
+        let input = self.load_input(false);
+        let original_map = self.build_map(input);
+        let mut valid_positions = 0;
+
+        for y in 0..original_map.map.len() {
+            for x in 0..original_map.map[0].len() {
+                if original_map.map[y][x] != '.'
+                    || (x as i32 == original_map.guard.coords.x
+                        && y as i32 == original_map.guard.coords.y)
+                {
+                    continue;
+                }
+
+                println!("Testing obstruction at position ({}, {})", x, y);
+
+                let obstruction = Point {
+                    x: x as i32,
+                    y: y as i32,
+                };
+
+                let mut test_map = original_map.clone();
+                test_map.map[y][x] = '#';
+
+                if self.simulate_guard(test_map, obstruction) {
+                    valid_positions += 1;
+                }
+            }
+        }
+
+        Some(valid_positions)
     }
 }
